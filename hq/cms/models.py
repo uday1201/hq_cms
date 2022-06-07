689f1adf0660016c9e8cae7da76da619158594d6
Uday Upreti <uday@Udays-MacBook-Pro.local>
2022-06-06 17:28:11 -0700

    models init

--- hq/cms/models.py
+++ hq/cms/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+
+class Assessment(models.Model):
+
+class Role(models.Model):
+
+class Exhibit(models.Model):
+
+class Excel(models.Model):
+
+class Role(models.Model):
+
+class QuesToRole(models.Model):
+    
